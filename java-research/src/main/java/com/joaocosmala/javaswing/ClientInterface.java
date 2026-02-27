392a532a614c9c35c72659c02de6dba8ba463230
joaosilveira <joaoguilhermesk8@hotmail.com>
2026-02-27 18:25:15 -0300

    chore: add new arq folder files

---
+++ java-research/src/main/java/com/joaocosmala/javaswing/ClientInterface.java
@@ -0,0 +1,50 @@
+package main.java.com.joaocosmala.javaswing;
+
+// importa awt[FlowLayout]
+// importa swing[JFrame, JButton, JLabel, JTextField]
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class ClientInterface extends JFrame {
+        // 1 - Declarar os componentes da interface gráfica.
+        JButton btnCadastrar;
+        JTextField txtNome, txtIdade;
+        JLabel lblNome, lblIdade;
+
+    public ClientInterface() {
+        setTitle("Exemplo de Cadastro");
+        setSize(600, 300);
+         // [Padrão para java.awt.Panel] Gerenciador de Layout (Atua apenas horizontalmente)
+         // Coloca os componentes sequencialmente da esquerda para a direita
+        setLayout(new FlowLayout());
+
+        // 2 - Construir o componente da interface gráfica.
+        btnCadastrar = new JButton("Cadastrar");
+        txtNome = new JTextField(20);
+        txtIdade = new JTextField(20);
+        lblNome = new JLabel("Nome:");
+        lblIdade = new JLabel("Idade:");
+
+        // 3 - Adicionar o componente da interface gráfica o JFrame.
+        add(lblNome);
+        add(txtNome);
+        add(lblIdade);
+        add(txtIdade);
+        add(btnCadastrar);
+
+        setVisible(true);
+        // setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+        // Executa a interface gráfica na thread de eventos.
+        ClientInterface frm = new ClientInterface();
+
+        // Configura a operação de fechamento da janela para encerrar o programa.
+        // Encerra o processo quando a janela for fechada. Não sobrecarrega.
+        frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
---
+++ java-research/src/main/java/com/joaocosmala/javaswing/ClientLayout.java
@@ -0,0 +1,69 @@
+package main.java.com.joaocosmala.javaswing;
+
+// Importa tudo[awt / swing]
+import java.awt.*;
+import javax.swing.*;
+
+public class ClientLayout extends JFrame {
+    // Passo 1
+    // --------------------------------- //
+    JButton btnCadastrar, btnAlterar, btnRemover, btnPesquisar;
+    JLabel lbCodigo, lbDescricao, lbQuantidade, lbLocalizacao;
+    JTextField txtCodigo, txtDescricao, txtQuantidade, txtLocalizacao;
+    JPanel pnCampos, pnBotoes;
+    
+    public ClientLayout() {
+        setTitle("Cadastro de Produto");
+        setSize(400, 300);
+
+    // Passo 2. Instanciação (Construção)
+    // --------------------------------- //
+        btnCadastrar = new JButton("Cadastrar");
+        btnAlterar = new JButton("Alterar");
+        btnRemover = new JButton("Remover");
+        btnPesquisar = new JButton("Pesquisar");
+
+        lbCodigo = new JLabel("Código");
+        lbDescricao = new JLabel("Descrição");
+        lbQuantidade = new JLabel("Quantidade");
+        lbLocalizacao = new JLabel("Localização");
+
+        txtCodigo = new JTextField(20);
+        txtDescricao = new JTextField(20);
+        txtQuantidade = new JTextField(20);
+        txtLocalizacao = new JTextField(20);
+
+        pnCampos = new JPanel(new FlowLayout());
+        pnBotoes = new JPanel(new FlowLayout());
+
+        setLayout(new FlowLayout());
+
+    // Passo 3. Adicionar os componentes em tela
+    // --------------------------------- //
+        pnCampos.add(lbCodigo);
+        pnCampos.add(txtCodigo);
+        pnCampos.add(lbDescricao);
+        pnCampos.add(txtDescricao);
+        pnCampos.add(lbQuantidade);
+        pnCampos.add(txtQuantidade);
+        pnCampos.add(lbLocalizacao);
+        pnCampos.add(txtLocalizacao);
+
+        pnBotoes.add(btnCadastrar);
+        pnBotoes.add(btnAlterar);
+        pnBotoes.add(btnRemover);
+        pnBotoes.add(btnPesquisar);
+
+        add(pnCampos, BorderLayout.CENTER);
+        add(pnBotoes, BorderLayout.SOUTH);
+
+        setVisible(true);
+        
+    }
+
+    public static void main(String[] args) {
+        ClientLayout frm = new ClientLayout();
+
+        frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
